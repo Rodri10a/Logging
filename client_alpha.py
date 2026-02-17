8457eb272af1a9bd98934160d3aaa0b8abbc4c27
Rodri10a <rodrigojosearguello@gmail.com>
2026-02-17 16:37:23 -0300

    feat: estructura base e imports

---
+++ client_alpha.py
@@ -0,0 +1,9 @@
+import requests
+import random
+from datetime import datetime, timezone
+
+# 🔐 Configuración del servicio
+TOKEN = "abc123"
+SERVICE_NAME = "service_alpha"
+URL = "http://localhost:5000/logs"
+
