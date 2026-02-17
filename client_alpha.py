e32b9d2d1474fba2a67532f2e8d4b6a121e45376
Rodri10a <rodrigojosearguello@gmail.com>
2026-02-17 16:53:23 -0300

    feat(client-alpha): constantes y datos de muestra

--- client_alpha.py
+++ client_alpha.py
@@ -2,8 +2,15 @@ import requests
 import random
 from datetime import datetime, timezone
 
-# 🔐 Configuración del servicio
 TOKEN = "abc123"
 SERVICE_NAME = "service_alpha"
 URL = "http://localhost:5000/logs"
 
+SEVERITIES = ["INFO", "DEBUG", "ERROR", "WARNING"]
+MESSAGES = [
+    "Todo bien.",
+    "Algo falló.",
+    "Conectando a base de datos.",
+    "Timeout inesperado.",
+    "Usuario no encontrado."
+]
